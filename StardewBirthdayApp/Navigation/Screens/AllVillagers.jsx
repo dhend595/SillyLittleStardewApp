b4969b5b3d91f9298383a8489aef2c65b01062e1
Doug Henderson <dhend595@gmail.com>
2023-05-15 13:06:59 -0400

    test image

--- StardewBirthdayApp/Navigation/Screens/AllVillagers.jsx
+++ StardewBirthdayApp/Navigation/Screens/AllVillagers.jsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import { View, Text, ImageBackground, Image } from 'react-native';
 import AbigailImage from '../../assets/villagers/Abigail.png'
 
 export default function AllVillagers({ navigation }) {
 
     // const [villagers, setVillagers] = useState([])
 
-    const image = {uri: 'https://reactjs.org/logo-og.png'};
+    const image = {
+        uri: 'StardewBirthdayApp/assets/villagers/Abigail.png',
+        width: 64,
+        height: 64,
+    };
 
 
     return (
@@ -14,7 +18,9 @@ export default function AllVillagers({ navigation }) {
             <Text
                 onPress={() => navigation.navigate('Home')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Villagers</Text>
-            <ImageBackground source={image}></ImageBackground>
+            {/* <Image source={{uri: `${AbigailImage}`}}/> */}
+            <Image source={{uri: 'https://reactjs.org/logo-og.png'}}
+       style={{width: 400, height: 400}} />
         </View>
     );
 }
